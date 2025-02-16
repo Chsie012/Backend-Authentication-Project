fe4b27f9f7dcf94d4c1240676c448a4334ec2acd
Caleb Hsieh <caleb060198@gmail.com>
2025-02-15 22:59:13 -0800

    Fixed the spelling

--- authenticate-me/backend/utils/validation.js
+++ authenticate-me/backend/utils/validation.js
@@ -8,10 +8,10 @@ const handleValidationErrors = (req, _res, next) => {
     if(!validationErrors.isEmpty()) {
         const errors = validationErrors.array().map((error) => `${error.msg}`);
 
-        const err = Error('Bad reqeust.');
+        const err = Error('Bad request.');
         err.errors = errors;
         err.status = 400;
-        err.title = 'Bad reqeust.';
+        err.title = 'Bad request.';
         next(err);
     }
     next();
